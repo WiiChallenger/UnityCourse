73b1a4f772213d4d680d56bc8cedb89567666cd8
WiiChallenger <tsomeone12@Gmail.com>
2025-10-03 23:09:23 -0500

    Fun habbits live strong
    GG's

--- Assets/Module2/GameObjective.cs
+++ Assets/Module2/GameObjective.cs
@@ -1,9 +1,11 @@
+using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameObjective : MonoBehaviour
 {
-    public string nextScene;
+    public string sceneName = "Level_1";
+    public int gamesComplete = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,9 +22,33 @@ public class GameObjective : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(nextScene);
+            CompletionTracker();
         }
     }
 
+    public void CompletionTracker()
+    {
+        // adds a key then gets the key adds it sets it then saves... 
+        string key = "Level_Complete"; 
+        gamesComplete = PlayerPrefs.GetInt(key, gamesComplete);
+        gamesComplete += 1;
+        PlayerPrefs.SetInt(key, gamesComplete);
+        PlayerPrefs.Save();
+        Debug.Log("Level complete" + gamesComplete);
 
+        if (gamesComplete > 2)
+        {
+            //returns the value to zero and owns the player :)
+            Debug.Log("Loading gag..");
+            gamesComplete = 0;
+            PlayerPrefs.SetInt(key, gamesComplete);
+            PlayerPrefs.Save();
+            SceneManager.LoadScene("Level_-0");
+            
+        }
+        else
+        {
+            SceneManager.LoadScene("Level_1");
+        }
+    }
 }
